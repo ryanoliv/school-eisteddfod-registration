3cfc6409e4b0cac75a3f9483ebf6d9ef4167b310
Ryan Oliver <ryanandrewoliver@gmail.com>
2025-03-14 08:26:45 +0200

    Add: faqs & footer

--- js/components.js
+++ js/components.js
@@ -1,8 +1,15 @@
-// components.js
 document.addEventListener("DOMContentLoaded", function () {
+  // Load Header
   fetch("components/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
+    });
+
+  // Load Footer
+  fetch("components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-container").innerHTML = data;
     });
 });
